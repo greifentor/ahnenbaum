e92d93e579671d90d29f10cc39916b11bc7a5c8b
ollie <lieshoff@gmx.de>
2024-11-30 15:00:13 +0100

    Set BRO's to generated.

--- service/src/main/java/de/ollie/ahnenbaum/backup/model/DatabaseBRO.java
+++ service/src/main/java/de/ollie/ahnenbaum/backup/model/DatabaseBRO.java
@@ -2,10 +2,12 @@ package de.ollie.ahnenbaum.backup.model;
 
 import java.util.List;
 import lombok.Data;
+import lombok.Generated;
 import lombok.experimental.Accessors;
 
 @Accessors(chain = true)
 @Data
+@Generated
 public class DatabaseBRO {
 
 	private List<PlaceBRO> places;
--- service/src/main/java/de/ollie/ahnenbaum/backup/model/PlaceBRO.java
+++ service/src/main/java/de/ollie/ahnenbaum/backup/model/PlaceBRO.java
@@ -2,10 +2,12 @@ package de.ollie.ahnenbaum.backup.model;
 
 import java.util.UUID;
 import lombok.Data;
+import lombok.Generated;
 import lombok.experimental.Accessors;
 
 @Accessors(chain = true)
 @Data
+@Generated
 public class PlaceBRO {
 
 	private UUID id;
